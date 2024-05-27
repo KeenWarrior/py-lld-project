9b567cdd6ee4bf3f79092c09e67245c34dc0c78b
Scaler <scaler@Anujs-MacBook-Pro.local>
2024-05-27 21:49:25 +0530

    L 8

---
+++ lecture-08/ecom/ecom/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class User(models.Model):
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class ShippingAddress(Address):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+
---
+++ lecture-08/ecom/ecom/views.py
@@ -0,0 +1,6 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
+from .models import Address, User
+
+
